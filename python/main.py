3c2328c641c43705a573675600721214c29d360c
Aditya Kumar Singh <50352988+TheMartian0x48@users.noreply.github.com>
2023-04-17 15:58:08 +0530

    implemented NotificationConfiguration (#2)
    Added desktop notification support

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@
 
 import subprocess, os, time, json, sys, random
 from pathlib import Path
+from enum import Enum
 import constants
 
 # CONFIG_FILE = "/.config/config_spanish_the_matian0x48.json"
@@ -21,39 +22,88 @@ class SingletonMeta(type):
 class AppConfiguration(metaclass=SingletonMeta):
     def __init__(self, configs: dict):
         self.configs = {}
-        self.required_fields = ["KNOWLEDGE_BASE_PATH"]
-        for field in self.required_fields:
+        required_fields = ["KNOWLEDGE_BASE_PATH"]
+        default_values = {
+            "SLEEP_TIME": 600
+        }
+        for field in required_fields:
             if configs.get(field) is None:
                 raise Exception(f"Need {field} for app configuration")
             else:
                 self.configs.setdefault(field, configs.get(field))
-        default_values = {
-            "SLEEP_TIME": 600
-        }
         for field in default_values:
             if configs.get(field) is None:
                 self.configs.setdefault(field, default_values.get(field))
             else:
                 self.configs.setdefault(field, configs.get(field))
 
-    def get_knowledge_base_path(self):
+    def get_knowledge_base_path(self) -> Path:
         return Path(self.configs.get("KNOWLEDGE_BASE_PATH"))
 
-    def get_sleep_time(self):
-        return self.configs.get("SLEEP_TIME")
+    def get_sleep_time(self) -> int:
+        return int(self.configs.get("SLEEP_TIME"))
 
 
-class NotificationConfiguration():
+class NotificationUrgencyLevel(Enum):
+    NORMAL = "NORMAL"
+    LOW = "LOW"
+    CRITICAL = "CRITICAL"
+
+
+class NotificationConfiguration:
     def __init__(self, configs: dict):
-        pass
+        self.APP_NAME = "APP_NAME"
+        self.URGENCY_LEVEL = "URGENCY_LEVEL"
+        self.ICON = "ICON"
+        self.EXPIRE_TIME = "EXPIRE_TIME"
+        self.configs = {}
+        required_fields = []
+        default_values = {
+            self.APP_NAME: "Knowledge Base",
+            self.ICON: None,
+            self.EXPIRE_TIME: 5000
+        }
+        for field in required_fields:
+            if configs.get(field) is None:
+                raise Exception(f"Need {field} for app configuration")
+            else:
+                self.configs.setdefault(field, configs.get(field))
+        if configs.get(self.URGENCY_LEVEL) is not None:
+            self.configs.setdefault(self.URGENCY_LEVEL, NotificationUrgencyLevel[configs.get(self.URGENCY_LEVEL)])
+        else:
+            self.configs.setdefault(self.URGENCY_LEVEL, NotificationUrgencyLevel.NORMAL)
+        for field in default_values:
+            if configs.get(field) is None:
+                self.configs.setdefault(field, default_values.get(field))
+            else:
+                self.configs.setdefault(field, configs.get(field))
+
+    def get_app_name(self) -> str:
+        return self.configs.get(self.APP_NAME)
+
+    def get_icon_path(self) -> str:
+        return self.configs.get(self.ICON)
+
+    def get_expire_time(self) -> int:
+        return int(self.configs.get(self.EXPIRE_TIME))
+
+    def get_urgency_level(self) -> NotificationUrgencyLevel:
+        return self.configs.get(self.URGENCY_LEVEL)
 
 
 class NotificationService(metaclass=SingletonMeta):
-    def __int__(self):
-        pass
+    def __init__(self):
+        self.service_provider = "notify-send"
 
-    def send(self, summary: str, message: str):
-        print('sending... send')
+    def send(self, config: NotificationConfiguration, summary: str, message: str):
+        # print('sending... send')
+        args = [self.service_provider,
+                f"--urgency={config.get_urgency_level().value}",
+                f"--expire-time={config.get_expire_time()}"]
+        if config.get_icon_path() is not None:
+            args.append(f"--icon={config.get_icon_path()}")
+        args = args + [summary, message]
+        subprocess.call(args)
 
 
 class KnowledgeBase(metaclass=SingletonMeta):
@@ -76,7 +126,7 @@ class KnowledgeBase(metaclass=SingletonMeta):
             self.modification_time = modification_time
         summary = random.choice(list(self.data.keys()))
         key = random.choice(list(self.data[summary].keys()))
-        return summary, f"{key}:{self.data[summary][key]}"
+        return summary, f"{key}\n:\n{self.data[summary][key]}"
 
 
 def get_configuration_file() -> str:
@@ -95,5 +145,5 @@ if __name__ == "__main__":
     knowledge_base = KnowledgeBase(app_config.get_knowledge_base_path())
     while True:
         summary, message = knowledge_base.get_summary_and_message()
-        notification_service.send(summary, message)
+        notification_service.send(notification_config, summary, message)
         time.sleep(app_config.get_sleep_time())
